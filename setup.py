22b485cc24541ae52530c0d762efbfbc19534d96
Aiman <aimanaw50@proton.me>
2025-05-04 15:51:14 +0200

    updated workflow

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version="0.1.2",
     packages=find_packages(),
     install_requires=[
-        "scikit-learn==1.3.0"
+        "scikit-learn==1.3.0",
+        "pandas>=1.5.0",
+        "numpy>=1.21.0",
+        "nltk==3.9",
+        "joblib>=1.1.0",
     ],
 )
