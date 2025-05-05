7c7d671e258b37acb8a9dd74918c792319b259f9
Aiman <aimanaw50@proton.me>
2025-05-05 17:09:14 +0200

    updated nltk version to 3.9.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="libml",
-    version="0.1.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=[
-        "scikit-learn==1.3.0",
+        "scikit-learn>=1.3.0",
         "pandas>=1.5.0",
         "numpy>=1.21.0",
-        "nltk==3.9",
+        "nltk>=3.9.1",
         "joblib>=1.1.0",
     ],
 )
